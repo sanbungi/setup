cd1558e578bd669557cfdd2f99a30a6f205fd3f6
Pikka2048 <0312pika0201@gmail.com>
2025-11-23 03:36:56 +0900

    rubyインストールスクリプトを追加

---
+++ install_ruby.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+sudo apt update
+sudo apt install -y libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev 
+
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+mkdir -p ~/.rbenv/plugins
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+
+rbenv install $(rbenv install -l | grep -v - | tail -1)
+
+ruby -v
