30466669a546d678989e16605a81f6d6b035e0e6
Pikka2048 <0312pika0201@gmail.com>
2025-11-23 04:01:54 +0900

    例外処理と細かい改善

--- install_ruby.sh
+++ install_ruby.sh
@@ -4,16 +4,30 @@ set -e
 sudo apt update
 sudo apt install -y libssl-dev libreadline-dev zlib1g-dev autoconf bison build-essential libyaml-dev libreadline-dev libncurses5-dev libffi-dev libgdbm-dev 
 
-git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+if [ ! -d "$HOME/.rbenv" ]; then
+  git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+else
+  echo "rbenv is already installed."
+fi
+
 mkdir -p ~/.rbenv/plugins
-git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+if [ ! -d "$HOME/.rbenv/plugins/ruby-build" ]; then
+  git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+fi
 
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-source ~/.bashrc
+# .bashrcに過剰な追記防止
+if ! grep -q "rbenv init" ~/.bashrc; then
+  echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+  echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+fi
 
+# 直接PATHを通して rbenv を有効化
+export PATH="$HOME/.rbenv/bin:$PATH"
+eval "$(rbenv init -)"
+
+# 最新のRubyをインストール
 rbenv install $(rbenv install -l | grep -v - | tail -1)
 
-sudo gem install solargraph
+gem install solargraph
 
 ruby -v
