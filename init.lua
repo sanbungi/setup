2eeff2e692048d684478f25cbb092d97aeb1fa65
sanbungi <0312pika0201@gmail.com>
2026-02-13 20:06:40 +0000

    cocからnvim-lsp + nvim-cmpに移行

--- init.lua
+++ init.lua
@@ -1,41 +1,39 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-        vim.fn.system({
-                "git",
-                "clone",
-                "--filter=blob:none",
-                "https://github.com/folke/lazy.nvim.git",
-                "--branch=stable",
-                lazypath,
-        })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 
 vim.opt.rtp:prepend(lazypath)
 local opt = vim.opt
 
 opt.number = true
-opt.cursorline = true -- カーソル行をハイライト
-opt.termguicolors = true -- True Colorを使用
+opt.cursorline = true
+opt.termguicolors = true
 
 if vim.fn.has("persistent_undo") == 1 then
-        vim.opt.undodir = vim.fn.expand("~/.cache/nvim/undo")
-        vim.opt.undofile = true
+    vim.opt.undodir = vim.fn.expand("~/.cache/nvim/undo")
+    vim.opt.undofile = true
 end
 
-opt.tabstop = 4 -- タブ文字の表示幅
-opt.shiftwidth = 4 -- インデントの幅
-opt.expandtab = true -- タブをスペースに変換
-opt.autoindent = true -- 改行時に前の行のインデントを継続
-opt.smartindent = true -- C言語のようなスマートなインデント
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+opt.smartindent = true
 
--- 検索設定
-
-opt.ignorecase = true -- 大文字小文字を区別しない
-opt.smartcase = true -- 大文字が含まれる場合のみ区別する
-opt.wrapscan = true -- 最後まで検索したら先頭に戻る
-opt.hlsearch = true -- 検索結果をハイライト
-opt.incsearch = true -- 入力中に逐次検索を行う
+opt.ignorecase = true
+opt.smartcase = true
+opt.wrapscan = true
+opt.hlsearch = true
+opt.incsearch = true
 
 opt.helplang = "ja,en"
 vim.g.mapleader = " "
@@ -44,121 +42,314 @@ vim.g.maplocalleader = " "
 local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
--- ウィンドウ移動 (Ctrl + h/j/k/l)
+-- ウィンドウ移動
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
 keymap("n", "<C-l>", "<C-w>l", opts)
 
--- カーソル下のシンボルの定義場所へ移動
-keymap("n", "<leader>df", "<Plug>(coc-definition)", { silent = true })
--- カーソル下のシンボルが使われている箇所を一覧表示
-keymap("n", "<leader>rf", "<Plug>(coc-references)", { silent = true })
-
--- カーソル下のシンボルを一括リネーム
-keymap("n", "<leader>rn", "<Plug>(coc-rename)", { silent = true })
--- コード整形を実行
-keymap("n", "<leader>fmt", "<Plug>(coc-format)", { silent = true })
-
--- ファイル構造（関数・クラス一覧）を表示
-keymap("n", "<leader>o", ":CocList outline<CR>", { silent = true })
--- プロジェクト内のシンボル（関数・変数名など）を検索
-keymap("n", "<leader>s", ":CocList -I symbols<CR>", { silent = true })
-
--- 利用可能なコードアクション（クイックフィックス等）を表示
-keymap("n", "<leader>a", "<cmd>CocAction<cr>", { silent = true })
--- カーソル位置のシンボルの型情報やドキュメントを表示
-keymap("n", "<leader>h", "<cmd>call CocAction('doHover')<cr>", { silent = true })
-
-local opts2 = { silent = true, expr = true, noremap = true }
-keymap("i", "<Tab>", 'pumvisible() ? coc#_select_confirm() : "<Tab>"', opts2) -- Tabで補間を受け入れる
-keymap("i", "<CR>", 'coc#pum#visible() ? coc#pum#confirm() : "<CR>"', opts2) -- Enterで補完確定
-
-local opts3 = { silent = true }
-keymap("i", "<C-n>", "<Plug>(coc-next)", opts3) -- 下候補へ
-keymap("i", "<C-p>", "<Plug>(coc-prev)", opts3) -- 上候補へ
+-- バッファ移動（次 / 前）
+keymap("n", "<leader>n", "<cmd>bnext<CR>", opts)
+keymap("n", "<leader>p", "<cmd>bprevious<CR>", opts)
 
 -- screenとの干渉対策
 if vim.env.TERM:match("^screen") then
-        vim.opt.laststatus = 0
+    vim.opt.laststatus = 0
 end
 
 require("lazy").setup({
-        {
-                "navarasu/onedark.nvim",
-                priority = 1000,
-                config = function()
-                        require("onedark").setup({
-                                style = "darker",
-                        })
-                        require("onedark").load()
-                end,
-        },
-        {
-                "stevearc/conform.nvim",
-                event = { "BufReadPre", "BufNewFile" },
-                keys = {
-                        {
-                                -- shift + alt + f (alt + F)
-                                "<M-F>",
-                                function()
-                                        require("conform").format({ lsp_fallback = true })
-                                end,
-                                desc = "Format with conform.nvim",
-                        },
-                },
-                opts = {
-                        format_on_save = {
-                                lsp_fallback = true,
-                                timeout_ms = 1000,
-                        },
-                        formatters_by_ft = {
-                                lua = { "stylua" },
-                                python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
-                                go = { "goimports" },
-                                rust = { "rustfmt" },
-                                javascript = { "prettierd", "prettier", stop_after_first = true },
-                                typescript = { "prettierd", "prettier", stop_after_first = true },
-                                json = { "jq" },
-                                yaml = { "yamlfmt" },
-                                toml = { "taplo" },
-                                html = { "prettierd", "prettier" },
-                                css = { "prettierd", "prettier" },
-                                markdown = { "prettierd", "prettier" },
-                                sh = { "shfmt" },
-                                sql = { "sqlfluff" },
-                                xml = { "xmlformat" },
-                                dockerfile = { "hadolint" },
-                        },
-                },
-        },
+    -- カラースキーム
     {
-        'nvim-telescope/telescope.nvim', version = '*',
-        dependencies = {
-            'nvim-lua/plenary.nvim',
-            -- optional but recommended
-            { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
-        }
+        "navarasu/onedark.nvim",
+        priority = 1000,
+        config = function()
+            require("onedark").setup({
+                style = "darker",
+            })
+            require("onedark").load()
+        end,
     },
-        { "vim-jp/vimdoc-ja" },
-        { "neoclide/coc.nvim", branch = "release" },
+
+    -- Mason: LSPサーバー管理
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+
+    -- Mason-lspconfig: MasonとLSPの橋渡し
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+            "williamboman/mason.nvim",
+        },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "ts_ls",         -- TypeScript/JavaScript
+                    "pyright",       -- Python
+                    "jsonls",        -- JSON
+                    "html",          -- HTML
+                    "cssls",         -- CSS
+                    "gopls",         -- Go
+                    "rust_analyzer", -- Rust
+                    "lua_ls",        -- Lua
+                    "ruby_lsp",      -- Ruby
+                    "clangd",        -- C/C++
+                },
+                automatic_installation = true,
+            })
+        end,
+    },
+
+    -- nvim-lspconfig: デフォルト設定を提供（setup()は使わない）
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+        },
+        config = function()
+            -- Neovim 0.11の新しいAPIを使用
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+            -- 全サーバー共通の設定
+            vim.lsp.config('*', {
+                capabilities = capabilities,
+            })
+
+            -- Lua専用設定
+            vim.lsp.config('lua_ls', {
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = { "vim" },
+                        },
+                        workspace = {
+                            checkThirdParty = false,
+                        },
+                    },
+                },
+            })
+
+            -- サーバーを有効化（Neovim 0.11の新機能）
+            local servers = {
+                "ts_ls",
+                "pyright",
+                "jsonls",
+                "html",
+                "cssls",
+                "gopls",
+                "rust_analyzer",
+                "lua_ls",
+                "ruby_lsp",
+                "clangd",
+            }
+
+            vim.lsp.enable(servers)
+
+            -- LSPアタッチ時のキーマッピング
+            vim.api.nvim_create_autocmd("LspAttach", {
+                callback = function(args)
+                    local bufnr = args.buf
+
+                    -- 定義ジャンプ
+                    keymap("n", "<leader>df", vim.lsp.buf.definition, { buffer = bufnr, silent = true })
+
+                    -- 参照表示（Telescopeで）
+                    keymap("n", "<leader>rf", function()
+                        require("telescope.builtin").lsp_references()
+                    end, { buffer = bufnr, silent = true })
+
+                    -- リネーム
+                    keymap("n", "<leader>rn", vim.lsp.buf.rename, { buffer = bufnr, silent = true })
+
+                    -- フォーマット（conform.nvimに委譲）
+                    keymap("n", "<leader>fmt", function()
+                        require("conform").format({ lsp_fallback = true })
+                    end, { buffer = bufnr, silent = true })
+
+                    -- アウトライン表示（Telescopeで）
+                    keymap("n", "<leader>o", function()
+                        require("telescope.builtin").lsp_document_symbols()
+                    end, { buffer = bufnr, silent = true })
+
+                    -- シンボル検索（Telescopeで）
+                    keymap("n", "<leader>s", function()
+                        require("telescope.builtin").lsp_dynamic_workspace_symbols()
+                    end, { buffer = bufnr, silent = true })
+
+                    -- コードアクション
+                    keymap("n", "<leader>a", vim.lsp.buf.code_action, { buffer = bufnr, silent = true })
+
+                    -- ホバー情報
+                    keymap("n", "<leader>h", vim.lsp.buf.hover, { buffer = bufnr, silent = true })
+
+                    -- 診断メッセージ表示
+                    keymap("n", "<leader>d", vim.diagnostic.open_float, { buffer = bufnr, silent = true })
+
+                    -- 標準的なキーマッピングも追加
+                    keymap("n", "gd", vim.lsp.buf.definition, { buffer = bufnr, silent = true })
+                    keymap("n", "gr", vim.lsp.buf.references, { buffer = bufnr, silent = true })
+                    keymap("n", "K", vim.lsp.buf.hover, { buffer = bufnr, silent = true })
+
+                    -- 診断の移動（次/前）
+                    keymap("n", "[d", vim.diagnostic.goto_prev,
+                        { buffer = bufnr, silent = true, desc = "Prev diagnostic" })
+                    keymap("n", "]d", vim.diagnostic.goto_next,
+                        { buffer = bufnr, silent = true, desc = "Next diagnostic" })
+
+                    -- 宣言へ（definitionの1段上、言語によっては有用）
+                    keymap("n", "gD", vim.lsp.buf.declaration,
+                        { buffer = bufnr, silent = true, desc = "Go to declaration" })
+
+                    -- 実装へ
+                    keymap("n", "gi", vim.lsp.buf.implementation,
+                        { buffer = bufnr, silent = true, desc = "Go to implementation" })
+
+                    -- 型定義へ（Neovim APIは type_definition）
+                    keymap("n", "gt", vim.lsp.buf.type_definition,
+                        { buffer = bufnr, silent = true, desc = "Go to type definition" })
+
+                    -- シグネチャヘルプ（引数ヒント）
+                    keymap("n", "<C-k>", vim.lsp.buf.signature_help,
+                        { buffer = bufnr, silent = true, desc = "Signature help" })
+                end,
+            })
+
+            -- 診断表示の設定
+            vim.diagnostic.config({
+                virtual_text = true,
+                signs = true,
+                underline = true,
+                update_in_insert = false,
+            })
+        end,
+    },
+
+    -- nvim-cmp: 補完エンジン
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+        },
+        config = function()
+            local cmp = require("cmp")
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    -- Tabで補完確定
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.confirm({ select = true })
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+
+                    -- Enterでも補完確定
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+
+                    -- Ctrl-nで次の候補、Ctrl-pで前の候補
+                    ["<C-n>"] = cmp.mapping.select_next_item(),
+                    ["<C-p>"] = cmp.mapping.select_prev_item(),
+
+                    -- Ctrl-Spaceで補完メニュー表示
+                    ["<C-Space>"] = cmp.mapping.complete(),
+
+                    -- Ctrl-eでキャンセル
+                    ["<C-e>"] = cmp.mapping.abort(),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "buffer" },
+                    { name = "path" },
+                }),
+                completion = {
+                    completeopt = "menu,menuone,noinsert",
+                },
+                performance = {
+                    max_view_entries = 50,
+                },
+            })
+        end,
+    },
+
+    -- LuaSnip: スニペットエンジン
+    {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+    },
+
+    -- Conform: フォーマッター
+    {
+        "stevearc/conform.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        keys = {
+            {
+                "<M-F>",
+                function()
+                    require("conform").format({ lsp_fallback = true })
+                end,
+                desc = "Format with conform.nvim",
+            },
+        },
+        opts = {
+            format_on_save = {
+                lsp_fallback = true,
+                timeout_ms = 1000,
+            },
+            formatters_by_ft = {
+                lua = { "stylua" },
+                python = { "ruff_fix", "ruff_format", "ruff_organize_imports" },
+                go = { "goimports" },
+                rust = { "rustfmt" },
+                javascript = { "prettierd", "prettier", stop_after_first = true },
+                typescript = { "prettierd", "prettier", stop_after_first = true },
+                json = { "jq" },
+                yaml = { "yamlfmt" },
+                toml = { "taplo" },
+                html = { "prettierd", "prettier" },
+                css = { "prettierd", "prettier" },
+                markdown = { "prettierd", "prettier" },
+                sh = { "shfmt" },
+                sql = { "sqlfluff" },
+                xml = { "xmlformat" },
+                dockerfile = { "hadolint" },
+            },
+        },
+    },
+
+    -- Telescope
+    {
+        "nvim-telescope/telescope.nvim",
+        version = "*",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        },
+        config = function()
+            require("telescope").setup({})
+            pcall(require("telescope").load_extension, "fzf")
+
+            local builtin = require("telescope.builtin")
+            keymap("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+            keymap("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
+            keymap("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
+            keymap("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+        end,
+    },
+
+    -- 日本語ヘルプ
+    { "vim-jp/vimdoc-ja" },
 })
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-
-vim.g.coc_global_extensions = {
-        "coc-tsserver",
-        "coc-pyright",
-        "coc-json",
-        "coc-html",
-        "coc-css",
-        "coc-go",
-        "coc-rust-analyzer",
-        "coc-lua",
-        "coc-solargraph",
-        "coc-go",
-}
